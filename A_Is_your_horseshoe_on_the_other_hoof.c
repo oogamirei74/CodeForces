2a75b9edd942b6100ee680da30e7e17b4a1407ae
Karim Eshat <146974243+oogamirei74@users.noreply.github.com>
2025-06-01 13:03:11 +0600

    Add files via upload

---
+++ A_Is_your_horseshoe_on_the_other_hoof.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+int  main()
+ 
+{
+    int cnt = 0;
+    int s1, s2, s3, s4;
+ 
+    scanf("%d", &s1);
+    scanf("%d", &s2);
+    if(s1 == s2){
+    cnt = cnt+1;
+    }
+    scanf("%d", &s3);
+    if(s3 == s2 || s3 == s1){
+    cnt = cnt+1;
+    }
+    scanf("%d", &s4);
+    if(s4 == s3 || s4 == s2 || s4 == s1){
+    cnt = cnt+1;
+    }
+ 
+    printf("%d", cnt);
+ 
+    return 0;
+#include<stdio.h>
+int  main()
+ 
+{
+    int cnt = 0;
+    int s1, s2, s3, s4;
+ 
+    scanf("%d", &s1);
+    scanf("%d", &s2);
+    if(s1 == s2){
+    cnt = cnt+1;
+    }
+    scanf("%d", &s3);
+    if(s3 == s2 || s3 == s1){
+    cnt = cnt+1;
+    }
+    scanf("%d", &s4);
+    if(s4 == s3 || s4 == s2 || s4 == s1){
+    cnt = cnt+1;
+    }
+ 
+    printf("%d", cnt);
+ 
+    return 0;
+}
